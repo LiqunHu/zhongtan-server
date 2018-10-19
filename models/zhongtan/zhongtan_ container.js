7dfe46fb79b224f34c20749b3e41444d4bfdef5f
liugd <540301291@qq.com>
2018-10-19 14:19:25 +0800

    集装箱表

--- models/zhongtan/zhongtan_ container.js
+++ models/zhongtan/zhongtan_ container.js
@@ -12,13 +12,55 @@ module.exports = db.defineModel('tbl_zhongtan_container', {
   container_type: { // 集装箱类型
     type: db.STRING(20)
   },
-  container_size: { // 集装箱大小
+  container_no: { // 箱号
     type: db.STRING(20)
   },
   container_iso: { // 集装箱ISO 型号
     type: db.STRING(20)
   },
-  container_gross_weight: { // 毛重
+  container_size: { // 集装箱大小
+    type: db.STRING(20)
+  },
+  container_type: { // 集装箱类型
+    type: db.STRING(20)
+  },
+  container_seal_no1: { // 封号1
+    type: db.STRING(20)
+  },
+  container_seal_no2: { // 封号2
+    type: db.STRING(20)
+  },
+  container_seal_no3: { // 封号3
+    type: db.STRING(20)
+  },
+  container_freight_indicator: { // 运费指示器
+    type: db.STRING(20)
+  },
+  container_package_no: { // 包装数
+    type: db.INTEGER(20)
+  },
+  container_package_unit: { // 包装类型
+    type: db.STRING(20)
+  },
+  container_volume: { // 体积
+    type: db.INTEGER(20)
+  },
+  container_volume_unit: { // 体积单位
+    type: db.STRING(20)
+  },
+  container_weight: { // 重量
+    type: db.INTEGER(20)
+  },
+  container_weight_unit: { // 重量单位
+    type: db.STRING(20)
+  },
+  container_minmum_temperature: { // 最低温度
+    type: db.INTEGER(20)
+  },
+  container_maxmum_temperature: { // 最高温度
+    type: db.INTEGER(20)
+  },
+  container_refer_plug: { // 
     type: db.STRING(20)
   }
 });
