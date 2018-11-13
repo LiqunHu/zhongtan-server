62d5fb8dcb6a61a6d27fad28ceac018a0b87b262
huliqun <huliquns@126.com>
2018-11-13 18:24:44 +0800

    f

---
+++ models/zhongtan/zhongtan_sail_schedule_upload.js
@@ -0,0 +1,12 @@
+const db = require('../../util/db')
+
+module.exports = db.defineModel('tbl_zhongtan_sail_schedule_upload', {
+  sail_schedule_upload_id: {
+    type: db.IDNO,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  sail_schedule_upload_desc: {
+    type: db.STRING(200)
+  }
+})
---
+++ models/zhongtan/zhongtan_uploadfile.js
@@ -0,0 +1,40 @@
+const db = require('../../util/db')
+// 提单表
+
+module.exports = db.defineModel('tbl_zhongtan_uploadfile', {
+  uploadfile_id: {
+    type: db.IDNO,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  api_name: {
+    type: db.STRING(100),
+    allowNull: false
+  },
+  user_id: {
+    type: db.ID,
+    allowNull: true
+  },
+  uploadfile_index1: {
+    type: db.IDNO,
+    allowNull: false
+  },
+  uploadfile_index2: {
+    type: db.IDNO,
+    allowNull: true
+  },
+  uploadfile_index3: {
+    type: db.IDNO,
+    allowNull: true
+  },
+  uploadfile_name: {
+    type: db.STRING(200),
+    defaultValue: '',
+    allowNull: true
+  },
+  uploadfile_url: {
+    type: db.STRING(500),
+    defaultValue: '',
+    allowNull: true
+  }
+})
