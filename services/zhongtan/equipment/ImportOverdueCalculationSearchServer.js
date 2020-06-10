d0f972cc5c569fbcd962075db4d39dfc2620ea3e
liugd <540301291@qq.com>
2020-06-10 10:37:08 +0800

    invoice search

--- services/zhongtan/equipment/ImportOverdueCalculationSearchServer.js
+++ services/zhongtan/equipment/ImportOverdueCalculationSearchServer.js
@@ -23,17 +23,18 @@ exports.searchAct = async req => {
   let doc = common.docValidate(req)
   let returnData = {}
 
-  let queryStr = `SELECT a.*, b.invoice_vessel_name, b.invoice_vessel_voyage, b.invoice_vessel_ata, b.invoice_vessel_atd, b.invoice_vessel_eta, c.invoice_masterbi_id, c.invoice_masterbi_cargo_type, c.invoice_masterbi_destination from tbl_zhongtan_invoice_containers a LEFT JOIN tbl_zhongtan_invoice_vessel b ON a.invoice_vessel_id = b.invoice_vessel_id AND b.state = '1' LEFT JOIN tbl_zhongtan_invoice_masterbl c ON a.invoice_containers_bl = c.invoice_masterbi_bl AND c.state = '1' AND c.invoice_vessel_id = a.invoice_vessel_id WHERE a.state = '1' AND c.invoice_masterbi_id IN (SELECT u.uploadfile_index1 FROM tbl_zhongtan_uploadfile u WHERE u.api_name = 'OVERDUE-INVOICE' AND (u.uploadfile_receipt_no IS NULL OR u.uploadfile_receipt_no = '')) `
+  let queryStr = `SELECT a.*, b.invoice_vessel_name, b.invoice_vessel_voyage, b.invoice_vessel_ata, b.invoice_vessel_atd, b.invoice_vessel_eta, c.invoice_masterbi_id, c.invoice_masterbi_cargo_type, c.invoice_masterbi_destination from tbl_zhongtan_invoice_containers a LEFT JOIN tbl_zhongtan_invoice_vessel b ON a.invoice_vessel_id = b.invoice_vessel_id AND b.state = '1' LEFT JOIN tbl_zhongtan_invoice_masterbl c ON a.invoice_containers_bl = c.invoice_masterbi_bl AND c.state = '1' AND c.invoice_vessel_id = a.invoice_vessel_id WHERE a.state = '1' `
   let replacements = []
-
+  if(doc.search_data && doc.search_data.date && doc.search_data.date.length > 1) {
+    let start_date = doc.search_data.date[0]
+    let end_date = doc.search_data.date[1]
+    queryStr += ` AND c.invoice_masterbi_id IN (SELECT u.uploadfile_index1 FROM tbl_zhongtan_uploadfile u WHERE u.api_name = 'OVERDUE-INVOICE' AND (u.uploadfile_receipt_no IS NULL OR u.uploadfile_receipt_no = '') AND u.created_at >= ? and u.created_at < ? ) `
+    replacements.push(start_date)
+    replacements.push(moment(end_date, 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD'))
+  } else {
+    queryStr += `AND c.invoice_masterbi_id IN (SELECT u.uploadfile_index1 FROM tbl_zhongtan_uploadfile u WHERE u.api_name = 'OVERDUE-INVOICE' AND (u.uploadfile_receipt_no IS NULL OR u.uploadfile_receipt_no = '')) `
+  }
   if(doc.search_data) {
-    if (doc.search_data.date && doc.search_data.date.length > 1) {
-      let start_date = doc.search_data.date[0]
-      let end_date = doc.search_data.date[1]
-      queryStr += ' and a.invoice_containers_empty_return_invoice_date >= ? and a.invoice_containers_empty_return_invoice_date < ? '
-      replacements.push(start_date)
-      replacements.push(moment(end_date, 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD'))
-    }
     if (doc.search_data.invoice_containers_bl) {
       queryStr += ' and a.invoice_containers_bl like ? '
       replacements.push('%' + doc.search_data.invoice_containers_bl + '%')
