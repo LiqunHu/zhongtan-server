61fa7062d301390af84dc0b5dc26808c12b6feb2
刘冬 <>
2022-06-24 11:12:59 +0800

    modify

--- services/zhongtan/export/ShipmentReleaseServer.js
+++ services/zhongtan/export/ShipmentReleaseServer.js
@@ -271,7 +271,7 @@ exports.getBookingShipmentAct = async req => {
         }
       }
 
-      queryStr = `SELECT fee_data_code, fee_data_name, fee_data_enabled_start_date, fee_data_enabled_end_date FROM tbl_zhongtan_export_fee_data WHERE state = ? AND fee_data_payable_fixed = ? AND (fee_data_enabled_start_date IS NOT NULL OR fee_data_enabled_end_date IS NOT NULL) GROUP BY fee_data_code ORDER BY fee_data_id `
+      queryStr = `SELECT fee_data_code, fee_data_name, fee_data_enabled_start_date, fee_data_enabled_end_date FROM tbl_zhongtan_export_fee_data WHERE state = ? AND fee_data_payable = ? AND (fee_data_enabled_start_date IS NOT NULL OR fee_data_enabled_end_date IS NOT NULL) GROUP BY fee_data_code ORDER BY fee_data_id `
       replacements = [GLBConfig.ENABLE, GLBConfig.ENABLE]
       let etd_payable_fee = await model.simpleSelect(queryStr, replacements)
       if(etd_payable_fee) {
@@ -1379,9 +1379,9 @@ const calculationShipmentFee = async function(fee_data_code, shipment_fee_type, 
                 }
                 fee_currency = con_fee_data.fee_data_payable_amount_currency
                 if(export_masterbl_bl.indexOf('COSU') >= 0){
-                  shipment_fee_party = con_fee_data.fee_data_payable_cosco_party ? con_fee_data.fee_data_payable_cosco_party : con_fee_data.fee_data_payable_amount_currency
+                  shipment_fee_party = con_fee_data.fee_data_payable_cosco_party ? con_fee_data.fee_data_payable_cosco_party : con_fee_data.fee_data_payable_common_party
                 } else if(export_masterbl_bl.indexOf('OOLU') >= 0){
-                  shipment_fee_party = con_fee_data.fee_data_payable_oocl_party ? con_fee_data.fee_data_payable_oocl_party : con_fee_data.fee_data_payable_amount_currency
+                  shipment_fee_party = con_fee_data.fee_data_payable_oocl_party ? con_fee_data.fee_data_payable_oocl_party : con_fee_data.fee_data_payable_common_party
                 }
               }
             }
