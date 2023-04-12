50fbef68472254210c1f4cb18535e6e803b42fd2
刘冬 <>
2023-04-12 16:48:32 +0800

    modify

--- services/zhongtan/export/ShipmentReleaseServer.js
+++ services/zhongtan/export/ShipmentReleaseServer.js
@@ -33,20 +33,20 @@ exports.initAct = async () => {
   queryStr = `SELECT fee_data_code, fee_data_name FROM tbl_zhongtan_export_fee_data WHERE state = ? AND fee_data_receivable = ? AND (fee_data_receivable_fixed = ? OR fee_data_enabled_start_date IS NOT NULL OR fee_data_enabled_end_date IS NOT NULL) GROUP BY fee_data_code ORDER BY fee_data_id`
   replacements = [GLBConfig.ENABLE, GLBConfig.ENABLE, GLBConfig.ENABLE]
   let fixed_receivable_fee = await model.simpleSelect(queryStr, replacements)
-  returnData.FIXED_RECEIVABLE_FEE = fixed_receivable_fee
-  returnData.FIXED_RECEIVABLE_FEE.push({
+  fixed_receivable_fee.push({
     fee_data_code: 'RLC',
     fee_data_name: 'ROLLOVER CHARGE/SPACE LOSS'
   })
-  returnData.FIXED_RECEIVABLE_FEE.push({
+  fixed_receivable_fee.push({
     fee_data_code: 'BGF',
     fee_data_name: 'BOOKING GUARANTEE FEE'
   })
-  returnData.FIXED_RECEIVABLE_FEE.push({
+  fixed_receivable_fee.push({
     fee_data_code: 'EDF',
     fee_data_name: 'EXPORT DEPOSIT FEE'
   })
-  
+  returnData.FIXED_RECEIVABLE_FEE = _.uniqBy(fixed_receivable_fee, 'fee_data_code')
+
   queryStr = `SELECT fee_data_code, fee_data_name FROM tbl_zhongtan_export_fee_data WHERE state = ? AND fee_data_receivable = ? AND (fee_data_receivable_fixed = ? OR (fee_data_receivable_fixed = ? AND (fee_data_receivable_amount IS NULL OR fee_data_receivable_amount = ''))) GROUP BY fee_data_code ORDER BY fee_data_id`
   replacements = [GLBConfig.ENABLE, GLBConfig.ENABLE, GLBConfig.DISABLE, GLBConfig.ENABLE]
   let other_receivable_fee = await model.simpleSelect(queryStr, replacements)
